b01ce6860c988bb33ebee38f382a5c19dc07bd2e
Chris Franklin <cf73021@gmail.com>
2019-01-11 03:32:50 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get '/about', to: 'welcome#about'
+  root 'welcome#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
